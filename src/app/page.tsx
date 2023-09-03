82cd35704f33c076cc3369ee9306697f5b0cc4ff
OKUNOKENTARO <okunokentaro@me.com>
2023-09-03 11:35:52 +0900

    cosme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+import { ReactElement } from 'react';
+
 import { Sandbox } from '../client';
 import { getLocalIp } from '../server';
 
-export default function AppPage() {
-  return <Sandbox ip={getLocalIp()} />;
+export default function AppPage(): ReactElement {
+  const ip = getLocalIp();
+  return <Sandbox ip={ip} />;
 }
