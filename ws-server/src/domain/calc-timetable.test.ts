f944e8832ff824dcc32f079b1ff2b84fa5286cbf
OKUNOKENTARO <okunokentaro@me.com>
2023-09-11 22:29:57 +0900

    mandatoryBreakEvery

--- ws-server/src/domain/calc-timetable.test.ts
+++ ws-server/src/domain/calc-timetable.test.ts
@@ -6,13 +6,13 @@ type Entries = Parameters<typeof calcTimetable>[1];
 
 const min = 60 * 1000;
 
+const arena = {
+  firstPresentationStartTime: new Date('1970-01-01T00:00:00Z').valueOf(),
+  breakConfig: null,
+} as const satisfies Arena;
+
 describe('calcTimetable()', () => {
   describe('一人のとき', () => {
-    const arena = {
-      firstPresentationStartTime: new Date('1970-01-01T00:00:00Z').valueOf(),
-      breakDefault: null,
-    } as const satisfies Arena;
-
     test('case 01', () => {
       const expected = [
         {
@@ -49,12 +49,7 @@ describe('calcTimetable()', () => {
   });
 
   describe('複数人のとき', () => {
-    const arena = {
-      firstPresentationStartTime: new Date('1970-01-01T00:00:00Z').valueOf(),
-      breakDefault: null,
-    } as const satisfies Arena;
-
-    test('case 01', () => {
+    test('タイムテーブルを作成できる', () => {
       const expected = [
         {
           type: 'presentation',
@@ -75,6 +70,357 @@ describe('calcTimetable()', () => {
 
       const actual = calcTimetable(arena, entries);
       expect(actual).toEqual(expected);
+    });
+
+    describe('休憩を挿入する', () => {
+      test('case 01', () => {
+        const expected = [
+          {
+            type: 'presentation',
+            entryId: '1',
+            endAt: new Date('1970-01-01T00:10:00Z').valueOf(),
+          },
+          {
+            type: 'presentation',
+            entryId: '2',
+            endAt: new Date('1970-01-01T00:20:00Z').valueOf(),
+          },
+          {
+            type: 'presentation',
+            entryId: '3',
+            endAt: new Date('1970-01-01T00:30:00Z').valueOf(),
+          },
+          {
+            type: 'presentation',
+            entryId: '4',
+            endAt: new Date('1970-01-01T00:40:00Z').valueOf(),
+          },
+          {
+            type: 'presentation',
+            entryId: '5',
+            endAt: new Date('1970-01-01T00:50:00Z').valueOf(),
+          },
+          {
+            type: 'presentation',
+            entryId: '6',
+            endAt: new Date('1970-01-01T01:00:00Z').valueOf(),
+          },
+          {
+            type: 'break',
+            endAt: new Date('1970-01-01T01:10:00Z').valueOf(),
+          },
+          {
+            type: 'presentation',
+            entryId: '7',
+            endAt: new Date('1970-01-01T01:20:00Z').valueOf(),
+          },
+        ] as const satisfies ReturnType<typeof calcTimetable>;
+
+        const entries = [
+          { id: '1', duration: 10 * min },
+          { id: '2', duration: 10 * min },
+          { id: '3', duration: 10 * min },
+          { id: '4', duration: 10 * min },
+          { id: '5', duration: 10 * min },
+          { id: '6', duration: 10 * min },
+          { id: '7', duration: 10 * min },
+        ] as const satisfies Entries;
+
+        const actual = calcTimetable(
+          {
+            ...arena,
+            breakConfig: {
+              interval: 60 * min,
+              mandatoryBreakEvery: 60 * min,
+              duration: 10 * min,
+            },
+          },
+          entries,
+        );
+        expect(actual).toEqual(expected);
+      });
+
+      test('case 02', () => {
+        const expected = [
+          {
+            type: 'presentation',
+            entryId: '1',
+            endAt: new Date('1970-01-01T00:10:00Z').valueOf(),
+          },
+          {
+            type: 'presentation',
+            entryId: '2',
+            endAt: new Date('1970-01-01T00:20:00Z').valueOf(),
+          },
+          {
+            type: 'presentation',
+            entryId: '3',
+            endAt: new Date('1970-01-01T00:30:00Z').valueOf(),
+          },
+          {
+            type: 'presentation',
+            entryId: '4',
+            endAt: new Date('1970-01-01T00:40:00Z').valueOf(),
+          },
+          {
+            type: 'presentation',
+            entryId: '5',
+            endAt: new Date('1970-01-01T00:50:00Z').valueOf(),
+          },
+          {
+            type: 'break',
+            endAt: new Date('1970-01-01T01:00:00Z').valueOf(),
+          },
+          {
+            type: 'presentation',
+            entryId: '6',
+            endAt: new Date('1970-01-01T01:10:00Z').valueOf(),
+          },
+          {
+            type: 'presentation',
+            entryId: '7',
+            endAt: new Date('1970-01-01T01:20:00Z').valueOf(),
+          },
+        ] as const satisfies ReturnType<typeof calcTimetable>;
+
+        const entries = [
+          { id: '1', duration: 10 * min },
+          { id: '2', duration: 10 * min },
+          { id: '3', duration: 10 * min },
+          { id: '4', duration: 10 * min },
+          { id: '5', duration: 10 * min },
+          { id: '6', duration: 10 * min },
+          { id: '7', duration: 10 * min },
+        ] as const satisfies Entries;
+
+        const actual = calcTimetable(
+          {
+            ...arena,
+            breakConfig: {
+              interval: 50 * min,
+              mandatoryBreakEvery: 50 * min,
+              duration: 10 * min,
+            },
+          },
+          entries,
+        );
+        expect(actual).toEqual(expected);
+      });
+    });
+
+    test('休憩を2度以上挿入する', () => {
+      const expected = [
+        {
+          type: 'presentation',
+          entryId: '1',
+          endAt: new Date('1970-01-01T00:01:00Z').valueOf(),
+        },
+        {
+          type: 'presentation',
+          entryId: '2',
+          endAt: new Date('1970-01-01T00:02:00Z').valueOf(),
+        },
+        {
+          type: 'break',
+          endAt: new Date('1970-01-01T00:03:00Z').valueOf(),
+        },
+        {
+          type: 'presentation',
+          entryId: '3',
+          endAt: new Date('1970-01-01T00:04:00Z').valueOf(),
+        },
+        {
+          type: 'presentation',
+          entryId: '4',
+          endAt: new Date('1970-01-01T00:05:00Z').valueOf(),
+        },
+        {
+          type: 'break',
+          endAt: new Date('1970-01-01T00:06:00Z').valueOf(),
+        },
+        {
+          type: 'presentation',
+          entryId: '5',
+          endAt: new Date('1970-01-01T00:07:00Z').valueOf(),
+        },
+        {
+          type: 'presentation',
+          entryId: '6',
+          endAt: new Date('1970-01-01T00:08:00Z').valueOf(),
+        },
+        {
+          type: 'break',
+          endAt: new Date('1970-01-01T00:09:00Z').valueOf(),
+        },
+        {
+          type: 'presentation',
+          entryId: '7',
+          endAt: new Date('1970-01-01T00:10:00Z').valueOf(),
+        },
+      ] as const satisfies ReturnType<typeof calcTimetable>;
+
+      const entries = [
+        { id: '1', duration: min },
+        { id: '2', duration: min },
+        { id: '3', duration: min },
+        { id: '4', duration: min },
+        { id: '5', duration: min },
+        { id: '6', duration: min },
+        { id: '7', duration: min },
+      ] as const satisfies Entries;
+
+      const actual = calcTimetable(
+        {
+          ...arena,
+          breakConfig: {
+            interval: 2 * min,
+            mandatoryBreakEvery: 2 * min,
+            duration: min,
+          },
+        },
+        entries,
+      );
+      expect(actual).toEqual(expected);
+    });
+
+    describe('長い発表で休憩を確保できない場合は mandatoryBreakEvery の値に基づいて休憩を挿入', () => {
+      test('mandatoryBreakEvery null の場合', () => {
+        const expected = [
+          {
+            type: 'presentation',
+            entryId: '1',
+            endAt: new Date('1970-01-01T00:10:00Z').valueOf(),
+          },
+          {
+            type: 'presentation',
+            entryId: '2',
+            endAt: new Date('1970-01-01T00:20:00Z').valueOf(),
+          },
+          {
+            type: 'presentation',
+            entryId: '3',
+            endAt: new Date('1970-01-01T00:30:00Z').valueOf(),
+          },
+          {
+            type: 'presentation',
+            entryId: '4',
+            endAt: new Date('1970-01-01T00:40:00Z').valueOf(),
+          },
+          {
+            type: 'presentation',
+            entryId: '5',
+            endAt: new Date('1970-01-01T01:40:00Z').valueOf(),
+          },
+          {
+            type: 'break',
+            endAt: new Date('1970-01-01T01:50:00Z').valueOf(),
+          },
+          {
+            type: 'presentation',
+            entryId: '6',
+            endAt: new Date('1970-01-01T02:00:00Z').valueOf(),
+          },
+          {
+            type: 'presentation',
+            entryId: '7',
+            endAt: new Date('1970-01-01T02:10:00Z').valueOf(),
+          },
+        ] as const satisfies ReturnType<typeof calcTimetable>;
+
+        const entries = [
+          { id: '1', duration: 10 * min },
+          { id: '2', duration: 10 * min },
+          { id: '3', duration: 10 * min },
+          { id: '4', duration: 10 * min },
+          { id: '5', duration: 60 * min },
+          { id: '6', duration: 10 * min },
+          { id: '7', duration: 10 * min },
+        ] as const satisfies Entries;
+
+        const actual = calcTimetable(
+          {
+            ...arena,
+            breakConfig: {
+              interval: 45 * min,
+              mandatoryBreakEvery: null,
+              duration: 10 * min,
+            },
+          },
+          entries,
+        );
+        expect(actual).toEqual(expected);
+      });
+
+      test('mandatoryBreakEvery 1 以上 の場合', () => {
+        const expected = [
+          {
+            type: 'presentation',
+            entryId: '1',
+            endAt: new Date('1970-01-01T00:10:00Z').valueOf(),
+          },
+          {
+            type: 'presentation',
+            entryId: '2',
+            endAt: new Date('1970-01-01T00:20:00Z').valueOf(),
+          },
+          {
+            type: 'presentation',
+            entryId: '3',
+            endAt: new Date('1970-01-01T00:30:00Z').valueOf(),
+          },
+          {
+            type: 'presentation',
+            entryId: '4',
+            endAt: new Date('1970-01-01T00:40:00Z').valueOf(),
+          },
+          {
+            type: 'break',
+            endAt: new Date('1970-01-01T00:50:00Z').valueOf(),
+          },
+          {
+            type: 'presentation',
+            entryId: '5',
+            endAt: new Date('1970-01-01T01:50:00Z').valueOf(),
+          },
+          {
+            type: 'break',
+            endAt: new Date('1970-01-01T02:00:00Z').valueOf(),
+          },
+          {
+            type: 'presentation',
+            entryId: '6',
+            endAt: new Date('1970-01-01T02:10:00Z').valueOf(),
+          },
+          {
+            type: 'presentation',
+            entryId: '7',
+            endAt: new Date('1970-01-01T02:20:00Z').valueOf(),
+          },
+        ] as const satisfies ReturnType<typeof calcTimetable>;
+
+        const entries = [
+          { id: '1', duration: 10 * min },
+          { id: '2', duration: 10 * min },
+          { id: '3', duration: 10 * min },
+          { id: '4', duration: 10 * min },
+          { id: '5', duration: 60 * min },
+          { id: '6', duration: 10 * min },
+          { id: '7', duration: 10 * min },
+        ] as const satisfies Entries;
+
+        const actual = calcTimetable(
+          {
+            ...arena,
+            breakConfig: {
+              interval: 45 * min,
+              mandatoryBreakEvery: 60 * min,
+              duration: 10 * min,
+            },
+          },
+          entries,
+        );
+        expect(actual).toEqual(expected);
+      });
     });
   });
 });
--- ws-server/src/domain/calc-timetable.ts
+++ ws-server/src/domain/calc-timetable.ts
@@ -1,23 +1,14 @@
-import { assertExists } from 'universal/src';
+import { assertExists, exists } from 'universal/src';
 
-type Interval =
-  | Readonly<{
-      type: 'duration';
-      value: number; // ms
-    }>
-  | Readonly<{
-      type: 'count';
-      value: number; // 回
-    }>;
-
-type BreakDefault = Readonly<{
-  interval: Interval;
+type BreakConfig = Readonly<{
+  interval: number; // ms
+  mandatoryBreakEvery: number | null; // ms 少なくとも mandatoryBreakEvery 以内に休憩
   duration: number; // ms
 }>;
 
 type Arena = Readonly<{
   firstPresentationStartTime: number; // UnixTime ms
-  breakDefault: BreakDefault | null;
+  breakConfig: BreakConfig | null;
 }>;
 
 type Entry = Readonly<{
@@ -28,35 +19,99 @@ type Entry = Readonly<{
 const timetableTypes = ['presentation', 'break'] as const;
 type TimetableType = (typeof timetableTypes)[number];
 
-type Timetable = {
-  type: TimetableType;
-  entryId: string;
-  endAt: number; // UnixTime ms
-};
+type Timetable =
+  | Readonly<{
+      type: Extract<TimetableType, 'presentation'>;
+      entryId: string;
+      endAt: number; // UnixTime ms
+    }>
+  | Readonly<{
+      type: Extract<TimetableType, 'break'>;
+      endAt: number; // UnixTime ms
+    }>;
 
 export function calcTimetable(
   arena: Arena,
   entries: readonly Entry[],
 ): readonly Timetable[] {
+  type Temp = Readonly<{
+    timetable: ReturnType<typeof calcTimetable>;
+    sum: number;
+  }>;
+
   return entries.reduce(
-    (acc, v, i): ReturnType<typeof calcTimetable> => {
+    (acc, v, i, arr): Temp => {
       if (i === 0) {
-        return [
+        return {
+          timetable: [
+            {
+              type: 'presentation',
+              entryId: v.id,
+              endAt: arena.firstPresentationStartTime + v.duration,
+            },
+          ],
+          sum: v.duration,
+        };
+      }
+
+      const last = acc.timetable.at(-1) ?? null;
+      assertExists(last);
+
+      if (exists(arena.breakConfig) && arena.breakConfig.interval <= acc.sum) {
+        return {
+          timetable: [
+            ...acc.timetable,
+            {
+              type: 'break',
+              endAt: last.endAt + arena.breakConfig.duration,
+            },
+            {
+              type: 'presentation',
+              entryId: v.id,
+              endAt: last.endAt + arena.breakConfig.duration + v.duration,
+            },
+          ],
+          sum: v.duration,
+        };
+      }
+
+      const mandatoryBreakEvery =
+        arena.breakConfig?.mandatoryBreakEvery ?? Number.MAX_SAFE_INTEGER;
+
+      if (
+        i < arr.length - 1 &&
+        exists(arena.breakConfig) &&
+        mandatoryBreakEvery < acc.sum + arr[i + 1].duration
+      ) {
+        return {
+          timetable: [
+            ...acc.timetable,
+            {
+              type: 'presentation',
+              entryId: v.id,
+              endAt: last.endAt + v.duration,
+            },
+            {
+              type: 'break',
+              endAt: last.endAt + v.duration + arena.breakConfig.duration,
+            },
+          ],
+          sum: v.duration,
+        };
+      }
+
+      return {
+        timetable: [
+          ...acc.timetable,
           {
             type: 'presentation',
             entryId: v.id,
-            endAt: arena.firstPresentationStartTime + v.duration,
+            endAt: last.endAt + v.duration,
           },
-        ];
-      }
-
-      const last = acc.at(-1) ?? null;
-      assertExists(last);
-      return [
-        ...acc,
-        { type: 'presentation', entryId: v.id, endAt: last.endAt + v.duration },
-      ];
+        ],
+        sum: acc.sum + v.duration,
+      };
     },
-    [] as ReturnType<typeof calcTimetable>,
-  );
+    { timetable: [], sum: 0 } as Temp,
+  ).timetable;
 }
