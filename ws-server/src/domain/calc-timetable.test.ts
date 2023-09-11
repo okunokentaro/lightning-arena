c5230ded71473bf24ee29a5f81ae4d9cdbbdb508
OKUNOKENTARO <okunokentaro@me.com>
2023-09-11 22:54:01 +0900

    add tests

--- ws-server/src/domain/calc-timetable.test.ts
+++ ws-server/src/domain/calc-timetable.test.ts
@@ -495,6 +495,39 @@ describe('calcTimetable()', () => {
         );
         expect(actual).toEqual(expected);
       });
+
+      test('', () => {
+        const expected = [
+          {
+            type: 'presentation',
+            entryId: '1',
+            endAt: new Date('1970-01-01T00:50:00Z').valueOf(),
+          },
+          {
+            type: 'presentation',
+            entryId: '2',
+            endAt: new Date('1970-01-01T01:40:00Z').valueOf(),
+          },
+        ] as const satisfies ReturnType<typeof calcTimetable>;
+
+        const entries = [
+          { id: '1', duration: 50 * min },
+          { id: '2', duration: 50 * min },
+        ] as const satisfies Entries;
+
+        const actual = calcTimetable(
+          {
+            ...arena,
+            breakConfig: {
+              interval: 51 * min,
+              mandatoryBreakEvery: 100 * min,
+              duration: 10 * min,
+            },
+          },
+          entries,
+        );
+        expect(actual).toEqual(expected);
+      });
     });
   });
 });
